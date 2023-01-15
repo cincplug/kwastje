c321e0b1a0a9d667c6d474e5a8bd6767e94c2d95
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2023-01-15 20:56:59 +0100

    Extract SVG filters component

---
+++ src/Filters.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "./Kwastje.scss";
+
+const Filters = (props) => {
+  const { setup, h, x, y } = props;
+  return (
+    <>
+      {setup.erode && (
+        <filter id="erode-filter">
+          <feMorphology operator="erode" radius={setup.erode} />
+        </filter>
+      )}
+      {setup.dilate && (
+        <filter id="dilate-filter">
+          <feMorphology operator="dilate" radius={setup.dilate} />
+        </filter>
+      )}
+      <filter id="displacement-filter">
+        {setup.turbulence && (
+          <feTurbulence
+            type="turbulence"
+            baseFrequency={setup.thickness}
+            numOctaves={setup.displace}
+            result="turbulence-filter"
+          />
+        )}
+        {setup.displace && (
+          <feDisplacementMap
+            in2="turbulence-filter"
+            in="SourceGraphic"
+            scale={setup.displace}
+            xChannelSelector="R"
+            yChannelSelector="G"
+          />
+        )}
+      </filter>
+      <filter id="light-filter">
+        {setup.specular && (
+          <feSpecularLighting
+            result="light-filter"
+            specularConstant={setup.thickness}
+            specularExponent={setup.growth}
+            lightingColor={setup.fgColor}
+          >
+            <fePointLight x={x} y={y} z={setup.specular} />
+          </feSpecularLighting>
+        )}
+        {setup.diffuse && (
+          <feDiffuseLighting
+            result="light-filter"
+            lightingColor={setup.fgColor}
+          >
+            <feSpotLight
+              x={x}
+              y={y}
+              z={setup.diffuse}
+              limitingConeAngle={((y * 1110) / h) * 2}
+            />
+          </feDiffuseLighting>
+        )}
+        {setup.composite && (
+          <feComposite
+            in2="SourceGraphic"
+            in="light-filter"
+            operator="arithmetic"
+            k1="0"
+            k2={setup.composite}
+            k3="1"
+            k4="0"
+          />
+        )}
+      </filter>
+      {setup.blur && (
+        <filter id="blur-filter">
+          <feGaussianBlur in="SourceGraphic" stdDeviation={setup.blur} />
+        </filter>
+      )}
+      {setup.convolve && (
+        <filter id="convolve-filter">
+          <feConvolveMatrix
+            kernelMatrix={`${setup.convolve} 0 0
+                ${x} 0 0
+                0 0 ${setup.convolve}`}
+          />
+        </filter>
+      )}
+    </>
+  );
+};
+
+export default Filters;
--- src/Kwastje.js
+++ src/Kwastje.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import setupArray from "./_setup.json";
+import Filters from "./Filters";
 import "./Kwastje.scss";
 
 const Kwastje = () => {
@@ -30,14 +31,17 @@ const Kwastje = () => {
   function useAnimationFrame(callback) {
     const requestRef = useRef();
     const previousTimeRef = useRef();
-    const animate = useCallback((time) => {
-      if (previousTimeRef.current !== undefined) {
-        const deltaTime = time - previousTimeRef.current;
-        callback(deltaTime);
-      }
-      previousTimeRef.current = time;
-      requestRef.current = requestAnimationFrame(animate);
-    }, [callback]);
+    const animate = useCallback(
+      (time) => {
+        if (previousTimeRef.current !== undefined) {
+          const deltaTime = time - previousTimeRef.current;
+          callback(deltaTime);
+        }
+        previousTimeRef.current = time;
+        requestRef.current = requestAnimationFrame(animate);
+      },
+      [callback]
+    );
     useEffect(() => {
       requestRef.current = requestAnimationFrame(animate);
       return () => cancelAnimationFrame(requestRef.current);
@@ -429,88 +433,7 @@ const Kwastje = () => {
               transform={`rotate(${setup.angle} 0 0)`}
               transform-origin={"center"}
             >
-              {setup.erode && (
-                <filter id="erode-filter">
-                  <feMorphology operator="erode" radius={setup.erode} />
-                </filter>
-              )}
-              {setup.dilate && (
-                <filter id="dilate-filter">
-                  <feMorphology operator="dilate" radius={setup.dilate} />
-                </filter>
-              )}
-              <filter id="displacement-filter">
-                {setup.turbulence && (
-                  <feTurbulence
-                    type="turbulence"
-                    baseFrequency={setup.thickness}
-                    numOctaves={setup.displace}
-                    result="turbulence-filter"
-                  />
-                )}
-                {setup.displace && (
-                  <feDisplacementMap
-                    in2="turbulence-filter"
-                    in="SourceGraphic"
-                    scale={setup.displace}
-                    xChannelSelector="R"
-                    yChannelSelector="G"
-                  />
-                )}
-              </filter>
-              <filter id="light-filter">
-                {setup.specular && (
-                  <feSpecularLighting
-                    result="light-filter"
-                    specularConstant={setup.thickness}
-                    specularExponent={setup.growth}
-                    lightingColor={setup.fgColor}
-                  >
-                    <fePointLight x={x} y={y} z={setup.specular} />
-                  </feSpecularLighting>
-                )}
-                {setup.diffuse && (
-                  <feDiffuseLighting
-                    result="light-filter"
-                    lightingColor={setup.fgColor}
-                  >
-                    <feSpotLight
-                      x={x}
-                      y={y}
-                      z={setup.diffuse}
-                      limitingConeAngle={((y * 1110) / h) * 2}
-                    />
-                  </feDiffuseLighting>
-                )}
-                {setup.composite && (
-                  <feComposite
-                    in2="SourceGraphic"
-                    in="light-filter"
-                    operator="arithmetic"
-                    k1="0"
-                    k2={setup.composite}
-                    k3="1"
-                    k4="0"
-                  />
-                )}
-              </filter>
-              {setup.blur && (
-                <filter id="blur-filter">
-                  <feGaussianBlur
-                    in="SourceGraphic"
-                    stdDeviation={setup.blur}
-                  />
-                </filter>
-              )}
-              {setup.convolve && (
-                <filter id="convolve-filter">
-                  <feConvolveMatrix
-                    kernelMatrix={`${setup.convolve} 0 0
-                ${x} 0 0
-                0 0 ${setup.convolve}`}
-                  />
-                </filter>
-              )}
+              <Filters {...{ h, x, y, setup }} />
               {getPath(path)}
             </g>
           </svg>
