5903cf9c696cdf3bc52a0433bd78db734366e2d7
Luka Stanisavljevic <=>
2023-02-04 21:05:34 +0100

    Improve Bertje

--- src/kwastjes/Bertje.js
+++ src/kwastjes/Bertje.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Bertje = (props) => {
   const { setup, mouseX, mouseY, coords, commonProps } = props;
-  const [x1, y1] = coords;
-  const [x, y] = setup.isCentric ? coords : [mouseX, mouseY];
+  const [x1, y1] = setup.isCentric ? [mouseX, mouseY] : coords;
+  const [x, y] = coords;
 
   const x2 = Math.pow(x, setup.modifier);
-  const y2 = y + 20;
-  return <path d={`M${x1},${y1} L${x2},${y2}`} {...commonProps} />;
+  const y2 = y + 100;
+  return <path d={`M${x2},${y2} L${x1},${y1}`} {...commonProps} />;
 };
 
 export default Bertje;
