ba9a21448f120351a555bd3067fa4d3a21b856ee
Luka Stanisavljevic <=>
2023-03-01 10:47:22 +0100

    Add kwastjes

--- src/kwastjes/index.js
+++ src/kwastjes/index.js
@@ -3,10 +3,13 @@ import Colintje from "./Colintje";
 import Denisje from "./Denisje";
 import Laurtje from "./Laurtje";
 import Luukje from "./Luukje";
+import Roosje from "./Roosje";
 import Sybje from "./Sybje";
 import Sylweitje from "./Sylweitje";
 import Taimurtje from "./Taimurtje";
 import Timijtje from "./Timijtje";
+import Truusje from "./Truusje";
+import Wilempje from "./Wilempje";
 
 export const defaultKwastjeNames = [
   "Annijtje",
@@ -41,8 +44,11 @@ export const customKwastjes = {
   Denisje,
   Laurtje,
   Luukje,
+  Roosje,
   Sybje,
   Sylweitje,
   Taimurtje,
   Timijtje,
+  Truusje,
+  Wilempje,
 };
