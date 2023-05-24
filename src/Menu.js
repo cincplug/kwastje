4ec3085fbf1b95585169a7469953e747de1e594b
Luka Stanisavljevic <=>
2023-05-24 16:50:46 +0200

    Curb filters

--- src/Menu.js
+++ src/Menu.js
@@ -11,53 +11,48 @@ const Menu = (props) => {
     clear,
   } = props;
   return (
-    <>
-      <nav
-        className={`menu menu--controls menu--${menuVisibilityClass}`}
-        onClick={() => !isMenuVisible && setIsMenuVisible(true)}
-      >
+    <nav
+      className={`menu menu--controls menu--${menuVisibilityClass}`}
+      onClick={() => !isMenuVisible && setIsMenuVisible(true)}
+    >
+      <button
+        className="menu__toggle"
+        onClick={() => setIsMenuVisible(false)}
+        title="toggle"
+      ></button>
+      {getControls(setupArray.filter((control) => !control.isHidden))}
+      <div className="control" key="saveSvg">
         <button
-          className="menu__toggle"
-          onClick={() => setIsMenuVisible(false)}
-          title="toggle"
-        ></button>
-        {getControls(setupArray.filter((control) => !control.isFilter))}
-        <div className="control" key="saveSvg">
-          <button
-            className="control__input control__button control__button--save"
-            onClick={() => {
-              download();
-            }}
-          >
-            save
-          </button>
-        </div>
-        <div className="control" key="clear">
-          <button
-            className="control__input control__button control__button--clear"
-            onClick={() => {
-              clear();
-            }}
-          >
-            clear
-          </button>
-        </div>
-        <div className="control" key="reset">
-          <button
-            className="control__input control__button control__button--clear"
-            onClick={() => {
-              sessionStorage.clear();
-              window.location.reload();
-            }}
-          >
-            reset
-          </button>
-        </div>
-      </nav>
-      <nav className={`menu menu--filters menu--${menuVisibilityClass}`}>
-        {getControls(setupArray.filter((control) => control.isFilter))}
-      </nav>
-    </>
+          className="control__input control__button control__button--save"
+          onClick={() => {
+            download();
+          }}
+        >
+          save
+        </button>
+      </div>
+      <div className="control" key="clear">
+        <button
+          className="control__input control__button control__button--clear"
+          onClick={() => {
+            clear();
+          }}
+        >
+          clear
+        </button>
+      </div>
+      <div className="control" key="reset">
+        <button
+          className="control__input control__button control__button--clear"
+          onClick={() => {
+            sessionStorage.clear();
+            window.location.reload();
+          }}
+        >
+          reset
+        </button>
+      </div>
+    </nav>
   );
 };
 
