eb3928a6b329c98f86d6846646e445c1544bb3cd
Luka Stanisavljevic <=>
2023-07-08 21:38:46 +0200

    Improve blijpje

--- src/kwastjes/Blijpje.js
+++ src/kwastjes/Blijpje.js
@@ -13,32 +13,44 @@ const Blijpje = (props) => {
     commonProps,
   } = props;
 
-  const quadrantGap = setup.thickness * setup.growth;
-  const tickRadius = quadrantGap * (index + 1);
+  const qG = setup.thickness * setup.growth;
+  const tR = qG * (index / defaultX1 * defaultY1);
+  commonProps.transform = `translate(${defaultX1 - tR}, ${
+    defaultY1 - tR
+  })`;
   return (
-    <path
-      transform={`translate(${defaultX1 - tickRadius}, ${
-        defaultY1 - tickRadius
-      })`}
-      d={`M ${
-        tickRadius + quadrantGap / 2
-      } 0 A ${tickRadius} ${tickRadius} 0 0 1 ${
-        tickRadius * setup.modifier * 2
-      } ${tickRadius - quadrantGap / 2} M ${tickRadius * 2} ${
-        tickRadius + quadrantGap / 2
-      } A ${tickRadius} ${tickRadius} 0 0 1 ${
-        tickRadius * setup.modifier + quadrantGap / 2
-      } ${tickRadius * 2} M ${tickRadius - quadrantGap / 2} ${
-        tickRadius * 2
-      } A ${tickRadius} ${tickRadius} 0 0 1 0 ${
-        tickRadius * setup.modifier + quadrantGap / 2
-      } M 0 ${
-        tickRadius - quadrantGap / 2
-      } A ${tickRadius} ${tickRadius} 0 0 1 ${
-        tickRadius * setup.modifier - quadrantGap / 2
-      } 0`}
-      {...commonProps}
-    />
+    <>
+      <path
+        {...commonProps}
+        className={`radar__line radar__line--tick-${
+          index + 1
+        } radar__line--quadrant-1`}
+        d={`M ${tR + qG / 2} 0 A ${tR} ${tR} 0 0 1 ${tR * 2} ${tR - qG / 2}`}
+      />
+      <path
+        {...commonProps}
+        className={`radar__line radar__line--tick-${
+          index + 1
+        } radar__line--quadrant-2`}
+        d={`M ${tR * 2} ${tR + qG / 2} A ${tR} ${tR} 0 0 1 ${tR + qG / 2} ${
+          tR * 2
+        }`}
+      />
+      <path
+        {...commonProps}
+        className={`radar__line radar__line--tick-${
+          index + 1
+        } radar__line--quadrant-3`}
+        d={`M ${tR - qG / 2} ${tR * 2} A ${tR} ${tR} 0 0 1 0 ${tR + qG / 2}`}
+      />
+      <path
+        {...commonProps}
+        className={`radar__line radar__line--tick-${
+          index + 1
+        } radar__line--quadrant-4`}
+        d={`M 0 ${tR - qG / 2} A ${tR} ${tR} 0 0 1 ${tR - qG / 2} 0`}
+      />
+    </>
   );
 };
 
