fd7c166c419483dee4cb55e48c28102fbc4a859c
Luka Stanisavljevic <=>
2023-07-12 00:11:32 +0200

    Improve Ineke and Lonneke

--- src/kwastjes/Lonneke.js
+++ src/kwastjes/Lonneke.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Ineke from "./Ineke";
 
 const Lonneke = (props) => {
   const {
     setup,
     index,
     w,
-    h,
+    // h,
     defaultX1,
     defaultX2,
     defaultY1,
@@ -13,15 +14,19 @@ const Lonneke = (props) => {
     commonProps,
   } = props;
 
+  const inekeProps = {...props, setup: {...setup, modifier: defaultX1 / w}, commonProps: {...commonProps, strokeWidth: setup.thickness / 4}};
   return (
+    <>
+    <Ineke {...inekeProps}/>
     <polygon
       points={`${defaultX1},${defaultY1} ${
         Math.sin(defaultX1 * index) / setup.modifier
       },${Math.cos(defaultY1)} ${Math.sin(defaultX2 * index) + w / 2},${
-        Math.cos(defaultY2) + h
+        Math.cos(defaultY2)
       } ${defaultX2},${defaultY2}`}
       {...commonProps}
-    />
+      />
+      </>
   );
 };
 
