7bf087e2ff97a059f00599efcac3f4b150f34e56
Luka Stanisavljevic <=>
2023-07-11 02:53:38 +0200

    Revolutionize Redeblipje

--- src/kwastjes/Redeblijpje.js
+++ src/kwastjes/Redeblijpje.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Blijpje from "./Blijpje";
+import Deblijpje from "./Deblijpje";
 
 const Redeblijpje = (props) => {
   const {
@@ -15,18 +16,33 @@ const Redeblijpje = (props) => {
   } = props;
 
   const qG = setup.thickness * setup.growth;
-  const tR = qG * index - (defaultX2 + defaultY2) / 2;
+  const tR = qG * index - (defaultX2 + defaultY2) / 3;
   return (
-    <Blijpje
-      {...{
-        setup,
-        index,
-        defaultX1,
-        defaultY1,
-        commonProps,
-        tR,
-      }}
-    />
+    <>
+      {index % 2 === 0 ? (
+        <Blijpje
+          {...{
+            setup,
+            index: index - 10,
+            defaultX1,
+            defaultY1: defaultY1 - 100,
+            commonProps,
+            tR,
+          }}
+        />
+      ) : (
+        <Deblijpje
+          {...{
+            setup,
+            index: (defaultX2 + defaultY2) / 100,
+            defaultX1,
+            defaultY1,
+            commonProps: {...commonProps, strokeWidth: setup.thickness / 4},
+            tR,
+          }}
+        />
+      )}
+    </>
   );
 };
 
