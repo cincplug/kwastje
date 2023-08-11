c88564676dfb8878db1565d19efd5a121c9bed28
Luka Stanisavljevic <=>
2023-08-11 16:01:48 +0200

    Refactor Blijpje

--- src/kwastjes/Blijpje.js
+++ src/kwastjes/Blijpje.js
@@ -14,50 +14,24 @@ const Blijpje = (props) => {
   } = props;
 
   const qG = setup.modifier * 10;
-  const tR = props.tR || qG * index / 2;
+  const tR = props.tR || (qG * index) / 2;
   return (
-    <g
-      {...commonProps}
-      transform={commonProps.normalize(`translate(${defaultX1 - tR}, ${defaultY1 - tR}) rotate(${index * setup.modifier})`)}
-    >
-      <path
-        className={`radar__line radar__line--tick-${
-          index + 1
-        } radar__line--quadrant-1`}
-        d={commonProps.normalize(
-          `M ${tR + qG / 2} 0 A ${tR} ${tR} 0 0 1 ${tR * 2} ${tR - qG / 2}`
-        )}
-      />
-      <path
-        {...commonProps}
-        className={`radar__line radar__line--tick-${
-          index + 1
-        } radar__line--quadrant-2`}
-        d={commonProps.normalize(
-          `M ${tR * 2} ${tR + qG / 2} A ${tR} ${tR} 0 0 1 ${tR + qG / 2} ${
-            tR * 2
-          }`
-        )}
-      />
-      <path
-        {...commonProps}
-        className={`radar__line radar__line--tick-${
-          index + 1
-        } radar__line--quadrant-3`}
-        d={commonProps.normalize(
-          `M ${tR - qG / 2} ${tR * 2} A ${tR} ${tR} 0 0 1 0 ${tR + qG / 2}`
-        )}
-      />
-      <path
-        {...commonProps}
-        className={`radar__line radar__line--tick-${
-          index + 1
-        } radar__line--quadrant-4`}
-        d={commonProps.normalize(
-          `M 0 ${tR - qG / 2} A ${tR} ${tR} 0 0 1 ${tR - qG / 2} 0`
-        )}
-      />
-    </g>
+    <>
+      {[0, 1, 2, 3].map((quadrant) => (
+        <path
+          {...commonProps}
+          transform={`translate(${defaultX1 - tR}, ${defaultY1 - tR}) rotate(${
+            quadrant * 90 + index * setup.modifier
+          }, ${tR}, ${tR})`}
+          className={`radar__line radar__line--tick-${
+            index + 1
+          } radar__line--quadrant-1`}
+          d={commonProps.normalize(
+            `M ${tR + qG / 2} 0 A ${tR} ${tR} 0 0 1 ${tR * 2} ${tR - qG / 2}`
+          )}
+        />
+      ))}
+    </>
   );
 };
 
