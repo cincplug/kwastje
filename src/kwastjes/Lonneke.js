d47506f80b847df9334619f88bb67c33108d4b7d
Luka Stanisavljevic <=>
2023-09-28 00:50:42 +0200

    Improve some more kwastjes

--- src/kwastjes/Lonneke.js
+++ src/kwastjes/Lonneke.js
@@ -1,25 +1,14 @@
 import React from "react";
 
 const Lonneke = (props) => {
-  const {
-    index,
-    w,
-    h,
-    x1,
-    x2,
-    y1,
-    y2,
-    commonProps,
-  } = props;
+  const { setup, index, w, h, x1, x2, y1, y2, commonProps } = props;
 
   return (
     <path
       d={commonProps.normalize(
-        `M${x1},${y1} C${Math.round(Math.sin(x1 * index) + w / 2)},${Math.round(
+        `M${x1},${y1} T${Math.round(Math.sin(x1 * index) + w / 2)},${Math.round(
           Math.cos(y1 * index) + h / 2
-        )} ${x1},${y2} ${Math.round(
-          Math.abs(x2 * Math.sqrt(index))
-        )},${Math.round(Math.cos(y2 * index + h / 2))}}`
+        )} ${x1 * setup.modifier},${(y2 * setup.modifier) / index}`
       )}
       {...commonProps}
     />
--- src/kwastjes/Maaike.js
+++ src/kwastjes/Maaike.js
@@ -3,7 +3,7 @@ import React from "react";
 const Maaike = (props) => {
   const { setup, index, w, h, x1, x2, y1, y2, commonProps } = props;
 
-  const spiralSegments = index;
+  const spiralSegments = Math.ceil((index + 1) / 6) * 6;
   const spiralPoints = Array.from({ length: spiralSegments }, (_, i) => {
     const angle = (i * Math.PI * 2) / spiralSegments;
     const radius = (Math.min(w, h) / 10) * setup.modifier;
@@ -15,7 +15,7 @@ const Maaike = (props) => {
   return (
     <path
       d={commonProps.normalize(
-        `M${(x2 + w) / 3},${y2} L${x1},${y1} Q${spiralPoints.join(" ")}`
+        `M${(x2 + w) / 3},${y2} L${x1},${y1} C${spiralPoints.join(" ")}`
       )}
       {...commonProps}
     />
