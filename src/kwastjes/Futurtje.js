7e12d5dfd20456c17c771a7a9465fb1cf3fe536b
Luka Stanisavljevic <=>
2023-10-02 17:02:23 +0200

    Improve the future

--- src/kwastjes/Futurtje.js
+++ src/kwastjes/Futurtje.js
@@ -1,26 +1,19 @@
 import React from "react";
 
 const Futurtje = (props) => {
-  const {
-    index,
-    w,
-    h,
-    x1,
-    y1,
-  } = props;
-
+  const { index, w, h, x1, y1 } = props;
+  const fontSize = y1 * index + h * 2;
   return (
-    <text className="future"
+    <text
+      className="future"
       x={(-x1 - w) / 2}
-      y={h}
-      fill={`${index % 2
-         === 0 ? "#2E308E" : "#FFFFFF"}70`}
-      stroke="none"
+      y={h + 3 * (index + 1)}
+      fill={`${index % 2 === 0 ? "#2E308E" : "#FFFFFF"}70`}
       style={{
-        fontSize: y1 / 2 * index,
+        fontSize,
       }}
     >
-      future
+      {`FUTURE`}
     </text>
   );
 };
