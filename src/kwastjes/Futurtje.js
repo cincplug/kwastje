4c985d6a5923943755e51ecb55aae5fcad522830
Luka Stanisavljevic <=>
2023-10-03 14:58:41 +0200

    Improve the future

--- src/kwastjes/Futurtje.js
+++ src/kwastjes/Futurtje.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Futurtje = (props) => {
-  const { index, w, h, x1, y1 } = props;
-  const fontSize = (Math.ceil(y1 * index + h));
+  const { setup, index, w, h, x1, y1 } = props;
+  const fontSize = Math.ceil((y1 * index + h) / 3);
   return (
     <text
       className="future"
-      x={Math.ceil((-x1 - w) / 2)}
+      x={Math.ceil((-x1 - w) / 2 + setup.modifier * (x1 + w) / 2)}
       y={h + (index + 1) * 2}
-      fill={`${index % 2 === 0 ? "#2E308E" : "#FFFFFF"}70`}
+      fill={`${index % 2 === 0 ? "#2E308E" : "#FFFFFF"}90`}
       style={{
         fontSize,
       }}
