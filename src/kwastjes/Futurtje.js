df7157c28d8249e82f311698ae3ed2e21e0b0771
Luka Stanisavljevic <=>
2023-10-05 08:56:09 +0200

    Align futurtje

--- src/kwastjes/Futurtje.js
+++ src/kwastjes/Futurtje.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Futurtje = (props) => {
-  const { setup, index, w, h, x1, y1 } = props;
+  const { setup, index, w, h, x1, y1, x2 } = props;
   const fontSize = Math.min(Math.ceil(y1 * index * setup.modifier), h * setup.growth);
   return (
     <text
       className="future"
-      x={Math.ceil((-x1 - w) / Math.max(setup.growth, 1) + setup.modifier * y1)}
-      y={h + ((index + 1) * Math.max(1, setup.thickness))}
+      x={Math.ceil((-x1 - w) / Math.max(setup.growth, 1) + setup.modifier * x2)}
+      y={h / 2 + ((index + 1) * Math.max(1, setup.thickness))}
       fill={`${index % 2 === 0 ? setup.fgColor : "#ffffff"}${setup.opacity.toString(16)}`}
       style={{
         fontSize,
