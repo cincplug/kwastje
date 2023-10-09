50adeee8ab17da028d4fbacba3b2edfd58b284a7
Luka Stanisavljevic <=>
2023-10-09 12:24:55 +0200

    Foster team spirit better

--- src/kwastjes/Truusje.js
+++ src/kwastjes/Truusje.js
@@ -18,10 +18,14 @@ const Truusje = (props) => {
   return (
     <path
       d={normalize(
-        `M${x1},${y1}  ${[...Array(index).keys()].map(
+        `M${x1},${y1}  ${[...Array(10).keys()].map(
           (step) =>
             ` ${
-              index > 30 ? `h${offset - step} v${offset / (step + 1)}` : ``
+              index < setup.dotsCount / 2
+                ? `l${offset - step},${offset - step}`
+                : `h${offset} m${offset},0 h${offset} m${
+                    -offset * 1.5
+                  },${offset} v${offset * 3} m${-offset},${-offset} a${offset},${offset} 0 1 0 ${2 * offset},0`
             } m${offset * 3},${Math.sin(index) * offset} h${
               -offset + step * 2
             }, v${-offset + step * 2}`
