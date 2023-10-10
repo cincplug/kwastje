a605ac5fa15f5b6ac1e41a516d478d95dead0159
Luka Stanisavljevic <=>
2023-10-10 08:58:22 +0200

    Fix path

--- src/kwastjes/Maurtje.js
+++ src/kwastjes/Maurtje.js
@@ -19,10 +19,7 @@ const Maurtje = (props) => {
       d={normalize(
         `M${x1 / 10},${x2 / setup.modifier - w / (index + 1)} L${x1},${
           y1 * setup.modifier
-        } h${x1 / (index + 1)} a10 ${index} 0 0 1 ${x1 / (index + 1)},${
-          y1 / (index + 1)
-        } v${y1} a${index} 10 0 0 1 ${x1 / (index + 1)}
-        } Z`
+        } a${setup.modifier * 10} ${index} 0 0 1 ${x1 / (index + 1)} ${y1 / (index + 1)} v${y1}`
       )}
       {...commonProps}
     />
