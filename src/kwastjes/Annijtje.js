4813deba66d0682aa89eec69e434373760c05a59
Luka Stanisavljevic <=>
2023-10-10 13:54:11 +0200

    Redefine the creativity and directions

--- src/kwastjes/Annijtje.js
+++ src/kwastjes/Annijtje.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Annijtje = (props) => {
   const {
-    // setup,
+    setup,
     index,
     w,
     h,
@@ -18,11 +18,11 @@ const Annijtje = (props) => {
     <path
       d={normalize(
         `M${x1},${y2} C
-        ${Math.min(w / 2, y2 * (index + 1))},
-        ${Math.min(h / 2, x2 * (index + 1))}
-         ${Math.min(w / 2, x1 * (index + 1))},
+        ${Math.min(w / 2, y2 / (index + 1))},
+        ${Math.min(h / 2, x2 * (index + 1)) / 2 + index ** setup.modifier}
+         ${Math.min(w / 2, x1 * (index + 1)) + index * 10},
          ${y1} 
-         ${x2},${y2} z`
+         ${x2},${y2}`
       )}
       {...commonProps}
     />
--- src/kwastjes/Kieke.js
+++ src/kwastjes/Kieke.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 const Kieke = (props) => {
-  const { index, setup, x1, x2, y1, y2, commonProps, normalize } = props;
+  const { index, setup, w, x1, x2, y1, y2, commonProps, normalize } = props;
 
   return (
     <path
       d={normalize(`M${x1},
       ${y1}
-      L${x2},
-      ${y2} m${index}, ${index * 5} l${x1 / (setup.dotsCount / setup.modifier - index)}, ${y1 / (setup.dotsCount / setup.modifier - index)} Z`)}
+       m${-index * 10}, ${index * 10} 
+      l${Math.max(
+        -w / 2,
+        Math.min(w / 2, x1 / (setup.dotsCount / setup.modifier - index * 2))
+      )}, ${y1 / (setup.dotsCount * setup.modifier - index)} L${x2},
+      ${y2} z`)}
       {...commonProps}
     />
   );
